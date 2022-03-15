73b1168ff50e9ad8b7cab523cf84fedd970f8d6c
edcilo <edcilop@gmail.com>
2022-03-15 11:20:59 -0600

    Add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:17-alpine
+
+ENV NODE_ENV=develop
+
+WORKDIR /app
+
+COPY ./package*.json .
+RUN npm install
+RUN npm install -g serve
+
+COPY . .
+RUN chown -R node:node /app
+RUN npm run build
+RUN chmod a+rx ./start.sh
+USER node
+
+EXPOSE 3000
+
+CMD ./start.sh
---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+echo $(date '+%F %T.%3N %Z') "[reactjs] INFO: running start.sh"
+
+env=${NODE_ENV:-develop}
+
+if [ $env = "production" ]
+then
+    echo $(date '+%F %T.%3N %Z') "[reactjs] INFO: running production environment"
+    echo $(date '+%F %T.%3N %Z') "[reactjs] INFO: serve application"
+    serve -s build
+else
+    echo $(date '+%F %T.%3N %Z') "[reactjs] INFO: running development environment"
+    npm run start
+fi
